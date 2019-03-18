c0d9a27f3ae4aeb8f3dade9d11994240d11c8591
AnshPrakash <anshprakash1997@gmail.com>
2019-03-18 22:53:31 +0530

    Sequential Code Done

--- LabAssignments/2/col380_lab2_suite-master/lab2_omp.cpp
+++ LabAssignments/2/col380_lab2_suite-master/lab2_omp.cpp
@@ -6,6 +6,24 @@
 #include <numeric>      // std::iota
 #include <algorithm>    // std::sort
 
+
+
+void print_matrix(float* Mat,int M,int N){
+	for (int i = 0; i < M; ++i){
+		for (int j = 0; j < N; ++j){
+			std::cout<<Mat[N*i+j]<<" ";
+		}
+		std::cout<<"\n";
+	}
+}
+
+void print_vectors(float* vec,int size){
+	for (int i = 0; i < size; ++i){
+		std::cout<<vec[i]<<" ";
+	}
+	std::cout<<"\n";
+}
+
 using namespace std;
 
 // Decesending Order
@@ -56,6 +74,7 @@ void QRfactorisations(float* A,float* Q,float* R,int n){
 	// we will do caculations in row major 
 	float* V_T=(float*)(malloc(sizeof(float)*n*n));
 	transpose(A,V_T,n,n);
+	// print_matrix(V_T,n,n);
 	float* Q_T=(float*)(malloc(sizeof(float)*n*n));
 	for (int i=0 ; i<n ; i++){
 		R[n*i+i]=norm_row_vec(V_T+n*i,n);//R[i][i]
@@ -63,20 +82,23 @@ void QRfactorisations(float* A,float* Q,float* R,int n){
 			Q_T[n*i+j] = V_T[n*i+j]/R[n*i+i];//R[i][i]
 		}
 		for (int j=i+1;j<n;j++){
-			R[n*i+j] = inner_product_rv(&Q_T[n*i],&V_T[n*j],n);
+			R[n*i+j] = inner_product_rv(Q_T+n*i,V_T+n*j,n);
 			for(int k=0;k<n;k++){
 				V_T[n*j+k] = V_T[n*j+k] -R[n*i+j]*Q_T[n*i+k];
 			}
 		}
 	}
 	transpose(Q_T,Q,n,n);
+	// std::cout<<"R\n";
+	// print_matrix(R,n,n);
 }
 
 void matmul(float* A,float* B,float* res,int a_m,int a_n,int b_m,int b_n){
 	for(int i=0;i<a_m;i++){
 		for(int j=0;j<b_n;j++){
+			res[b_n*i+j]=0;
 			for (int k = 0; k < a_n; ++k){
-				res[b_n*i+j] = A[a_n*i+k]*B[b_n*k+j];
+				res[b_n*i+j]+= A[a_n*i+k]*B[b_n*k+j];
 			}
 		}
 	}
@@ -91,14 +113,36 @@ bool converge(float* delta,int N){
 	return true;
 }
 
+void check_result(float *D,float* U,float* SIGMA,float* V_T,int M,int N){
+	float* sig_m=(float*)(malloc(sizeof(float)*N*M));
+	for (int i = 0; i < N; ++i)	{
+		for (int j = 0; j < M; ++j){
+			sig_m[M*i+j]=0;
+		}
+	}
+	for (int i = 0; i < N; ++i){
+		sig_m[M*i+i]=SIGMA[i];
+	}
+	float* temp=(float*)(malloc(sizeof(float)*N*M));
+	matmul(U,sig_m,temp,N,N,N,M);
+	float* temp2=(float*)(malloc(sizeof(float)*N*M));
+	matmul(temp,V_T,temp2,N,M,M,M);
+	std::cout<<"res \n";
+	print_matrix(temp2,N,M);
+	float* D_T=(float*)malloc(sizeof(float)*M*N);
+	transpose(D,D_T,M,N);
+	std::cout<<"D_T\n";
+	print_matrix(D_T,N,M);
+}
+
+
 // /*
 // 	*****************************************************
 // 		TODO -- You must implement this function
 // 	*****************************************************
 // */
 void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T){
-	float* SIGMA_p=*SIGMA;
-	float* V_T_p=*V_T;
+	// float* V_T_p=*V_T;
 	float* D_T =(float*)(malloc(sizeof(float)*N*M));
 	transpose(D,D_T,M,N);
 	float* A=(float*)(malloc(sizeof(float)*N*N));
@@ -107,18 +151,36 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T){
 	float* R=(float*)(malloc(sizeof(float)*N*N));
 	float* delta=(float*)(malloc(sizeof(float)*N));
 	float* res=(float*)(malloc(sizeof(float)*N*N));//temp var
+	float* A_temp=(float*)(malloc(sizeof(float)*N));
+	for (int i = 0; i < N; ++i){
+		for (int j = 0; j < N; ++j){
+			E[N*i+j]=0;
+			R[N*i+j]=0;
+		}
+	}
 	for (int i = 0; i < N; ++i)	{
 		delta[i]=10000;
+		A_temp[i]=1000000000;
+		E[N*i+i]=1;
 	}
 	matmul(D_T,D,A,N,M,M,N);
 	while(!converge(delta,N)){
 		QRfactorisations(A,Q,R,N);
 		matmul(R,Q,A,N,N,N,N);
 		matmul(E,Q,res,N,N,N,N);
-		for (int i = 0; i < N; ++i)	{
-			delta[i]=fabs(E[N*i+i]-res[N*i+i]);
+		// print_matrix(E,N,N);
+		for (int i = 0; i < N; ++i){
+			//delta[i]=fabs(E[N*i+i]-res[N*i+i]);
+			delta[i]=fabs(A_temp[i]-A[N*i+i]);
 		}
-		E=res;
+		for (int i = 0; i < N; ++i){
+			for (int j = 0; j < N; ++j){
+				E[N*i+j]=res[N*i+j];
+			}
+		}
+		for (int i = 0; i < N; ++i)	{
+			A_temp[i]=A[N*i+i];
+		}
 	}
 	for (int i = 0; i < N; ++i){
 		(*SIGMA)[i]=A[N*i+i];
@@ -135,17 +197,17 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T){
 	}
 	idx=0;
 	float* E_T=(float*)(malloc(sizeof(float)*N*N));
+	float* U_T=(float*)(malloc(sizeof(float)*N*N));
 	transpose(E,E_T,N,N);
 	for (auto i :sort_idx){
 		for (int j = 0; j < N; ++j){
-			(*U)[N*idx+j]=E_T[N*i+j];
+			(U_T)[N*idx+j]=E_T[N*i+j];
 		}
 		idx+=1;
 	}
 
 	float* sig_inv_ut=(float*)(malloc(sizeof(float)*M*N));//Sigma_inv.U_T
-	float* U_T=(float*)(malloc(sizeof(float)*N*N));
-	transpose((*U),U_T,N,N);
+	transpose(U_T,*U,N,N);
 	for (int i = 0; i < N; ++i){
 		for (int j = 0; j < N; ++j){
 			sig_inv_ut[N*i+j]=U_T[N*i+j]/(*SIGMA)[i];
@@ -157,7 +219,8 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T){
 		}	
 	}
 	matmul(sig_inv_ut,D_T,(*V_T),M,N,N,M);
-	
+
+	// check_result(D,*U,*SIGMA,*V_T,M,N);
 }
 
 // /*
@@ -172,7 +235,7 @@ void PCA(int retention, int M, int N, float* D, float* U, float* SIGMA, float** 
     	total[i]=SIGMA[i]+total[i-1];
     }
     *K=0;
-    for (int i = 0; i <N; ++i)    {
+    for (int i = 0; i <N; ++i){
     	*K=1+*K;
     	if (total[i]/total[N-1]>= retention){
     		break;
@@ -181,4 +244,9 @@ void PCA(int retention, int M, int N, float* D, float* U, float* SIGMA, float** 
     *D_HAT=(float*)malloc(sizeof(float)*M*(*K));
     matmul(D,U,(*D_HAT),M,N,N,*K);
 
+
+    // std::cout<<"U\n";
+    // print_matrix(U,N,N);
+    // std::cout<<"D_HAT\n";
+    // print_matrix((*D_HAT),M,*K);
 }
